8c5b70d09c21329da4e5f679e6f7f85e32be9f92
narminhasanova <narmin.sky@bk.ru>
2022-05-30 15:38:13 +0400

    Javascript Introduction

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log("salam Yer kuresi!!");
+// window.alert("Salam telebeler!");
+document.getElementById("demo").innerHTML = "Hello World!";
+document.write("30 May 2022-ci il");
+
+
+
+function imgShow() {
+    document.getElementById("photo").src = "pc.jpg";
+}
+
+//let firstname = prompt("buraya adinizi daxil edin:");
+
+/*console.log(firstname);
+
+document.getElementById("heading").innerText = firstname;
+window.alert(firstname); */
